e4efcda68fce0a19c68c5c24d26f91eefa4a6b81
heqiang <heqiang@qiniu.com>
2020-04-04 15:45:27 +0800

    first commit

---
+++ valid_parentheses.py
@@ -0,0 +1,30 @@
+#!/usr/env/bin python
+
+class Solution:
+
+    @classmethod
+    def valid(self, s):
+        result = []
+        kmap = {
+            "{": "}",
+            "[": "]",
+            "(": ")"
+        }
+
+        for x in s:
+            if x in kmap:
+                result.append(x)
+            else:
+                if len(result) != 0:
+                    if x != kmap[result.pop()]:
+                        return False
+                    else:
+                        continue
+                else:
+                    return False
+
+        return len(result) == 0
+
+
+if __name__ == '__main__':
+    print Solution.valid("{[()]}")
