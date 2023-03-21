05182ab19f38259c077ced8951055d71c74fe032
honorking <heqiang1068@gmail.com>
2023-03-21 11:55:10 +0800

    Update remove_element.py

--- remove_element.py
+++ remove_element.py
@@ -1,9 +1,9 @@
-def remove_element(list, num):
+def remove_element(nums, val):
     fast_index = 0
     slow_index = 0
-    while(fast_index<=len(list)-1):
-        if(num!=list[fast_index]):
-            list[slow_index++] = list[fast_index]
+    while(fast_index<=len(nums)):
+        if(val!=nums[fast_index]):
+            nums[slow_index++] = nums[fast_index]
         fast_index++
         
     return slow_index
